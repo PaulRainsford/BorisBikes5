0a7bb65852c1fd5d0820ca7466ce292a71a3d469
Paul Rainsford <paulrainsford69@gmail.co>
2021-07-30 17:35:05 +0100

    Ch 11 progress

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,15 +5,15 @@ describe DockingStation do
 
 it { is_expected.to respond_to :release_bike }
 
-it "release a bike" do
+it "release a bike and checks if working" do
 
 docking_station = DockingStation.new
 bike = docking_station.release_bike
 expect(bike).to be_working
 end
 
-it "is bike working" do
-expect(:working?).to eq true
+it { is_expected.to respond_to :dock_bike }
+it "docks a bike" do
+  expect(subject).to be_dock_bike
 end
-
 end
