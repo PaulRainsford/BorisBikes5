0212ecbd76d363e8729e232fcff1dd047321dbb6
Farya <farya.hsn@gmail.com>
2021-07-30 19:02:08 +0200

    ch 10 progress

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,21 @@
 
 class DockingStation
+
+ attr_reader :bikes, :bike
+ def initialize
+  @bikes = bike
+ end
+
   def release_bike
     Bike.new
   end
 
-
+  def dock_bike(bike)
+    @bikes = bike
+  end
 end
+
+
+# docking_station.release_bike = Bike.new
+# bike = Bike.new
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,7 +13,26 @@ expect(bike).to be_working
 end
 
 it { is_expected.to respond_to :dock_bike }
+
 it "docks a bike" do
-  expect(subject).to be_dock_bike
+  bike = Bike.new
+  expect(subject.dock_bike(bike)).to eq bike
 end
+
+it "checks if a bike has been docked" do
+bike = Bike.new
+p bikes
+expect(subject.dock_bike(bike)).to eq(bikes.length > 0)
 end
+
+end
+
+
+# station = DockingStation.new
+# station.dock_bike
+
+
+ # DockingStation.new 
+  # initialise @bikes = []
+  # .dock_bike(bike) bike = Bike.new
+  # @bikes.length > 0
